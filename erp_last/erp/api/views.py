9c99b036ae269545e89f7de151c65e0b10b7495b
jongho22 <sjh0804_22@naver.com>
2023-02-06 04:43:40 +0900

    제출_확인

--- erp_last/erp/api/views.py
+++ erp_last/erp/api/views.py
@@ -6,9 +6,9 @@ from rest_framework import generics
 
 from rest_framework import permissions
 # 인증 
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+#from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 # 권한
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+#from rest_framework.permissions import IsAuthenticatedOrReadOnly
 # 유저 확인
 #from ..permissions import IsOwenerOrReadOnly
 
